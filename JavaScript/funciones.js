b339726629f19665b65142869990c3e112397eed
Carolina Frogley <post.cf@gmail.com>
2023-01-21 20:51:22 -0300

    botones hechos

--- JavaScript/funciones.js
+++ JavaScript/funciones.js
@@ -2,3 +2,13 @@ document.getElementById('contacto').addEventListener('click', function(){
     console.log('El email fue enviado')
     document.getElementById('Gracias').style.display = 'block';
 })
+
+document.getElementById('telefono').addEventListener('click', function(){
+
+    document.getElementById('mostrar_tel').style.display = 'block';
+})
+
+document.getElementById('mail').addEventListener('click', function(){
+
+    document.getElementById('mostrar_mail').style.display = 'block';
+})
