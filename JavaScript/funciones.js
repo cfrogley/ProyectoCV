e1f5f955d7bf813e036a636733c623ccf141b7bb
Carolina Frogley <post.cf@gmail.com>
2023-01-21 20:40:31 -0300

    empezando con js

---
+++ JavaScript/funciones.js
@@ -0,0 +1,4 @@
+document.getElementById('contacto').addEventListener('click', function(){
+    console.log('El email fue enviado')
+    document.getElementById('Gracias').style.display = 'block';
+})
